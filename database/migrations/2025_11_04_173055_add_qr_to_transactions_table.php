30c1debd4f057043d0d1fd2c66411600816213da
Jonatas Santos <jonatasskt18@gmail.com>
2025-11-08 00:51:01 -0300

    refatoração: Boas Praticas com Exceptions, Policies e Services

--- database/migrations/2025_11_04_173055_add_qr_to_transactions_table.php
+++ database/migrations/2025_11_04_173055_add_qr_to_transactions_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            //
+            $table->dropColumn(['qr_payload', 'qr_image_url']);
         });
     }
 };
